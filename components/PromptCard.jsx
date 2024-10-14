60df902564aa2e2eff2edfaa9e5897680ef3c622
Aayush Sharma <aayushsharma47163@gmail.com>
2024-10-15 00:18:00 +0530

    Update PromptCard.jsx

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -30,13 +30,13 @@ const PromptCard = ({post, handleTagClick,handleEdit, handleDelete}) => {
     <div className="prompt_card">
     <div className="flex justify-between items-start gap-5">
       <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}>
-        <Image
+{/*         <Image
          src={post?.creator.image}
          alt = "user_image"
          width={40}
          height={40}
          className="rounded-full object-contain"
-        />
+        /> */}
         <div className="flex flex-col">
           <h3 className="font-satoshi font-semibold text-gray-900">
             {post.creator.username}
