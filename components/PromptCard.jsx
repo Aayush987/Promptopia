3a40ca0b169f987dc2c70f79aefdfcb64eaf3dfb
Aayush Sharma <aayushsharma47163@gmail.com>
2024-10-15 00:13:53 +0530

    Update PromptCard.jsx

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -14,7 +14,7 @@ const PromptCard = ({post, handleTagClick,handleEdit, handleDelete}) => {
   const handleProfileClick = () => {
     console.log(post);
 
-    if (post.creator._id === session?.user.id) return Router.push("/profile");
+    if (post?.creator._id === session?.user.id) return Router.push("/profile");
 
     Router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   };
@@ -31,7 +31,7 @@ const PromptCard = ({post, handleTagClick,handleEdit, handleDelete}) => {
     <div className="flex justify-between items-start gap-5">
       <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}>
         <Image
-         src={post.creator.image}
+         src={post?.creator.image}
          alt = "user_image"
          width={40}
          height={40}
