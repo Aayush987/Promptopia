893ec38858fa12a2d15d96984342060532dc08b4
Aayush Sharma <aayushsharma47163@gmail.com>
2024-10-17 18:49:55 +0530

    No Cache

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,9 @@ const Feed = () => {
    const [searchedResults, setSearchedResults] = useState([]);
 
    const fetchPosts = async () => {
-      const response = await fetch("/api/prompt");
+      const response = await fetch("/api/prompt", {
+        cache: 'no-store'
+      });
       const data = await response.json();
 
       setallPosts(data);
