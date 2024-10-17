4093649efd2640cd804776ecce78fa63b6620d3f
Aayush Sharma <aayushsharma47163@gmail.com>
2024-10-17 13:59:46 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const Home = async () => {
     cache: 'no-store'
    })
    const data = await fetchPosts.json();
-   console.log(data);
+   // console.log(data);
   return (
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
